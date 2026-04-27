06d78433916087abec13596c6fd8226cf1ff685a
Anna Axmatova <110549995+Annezi@users.noreply.github.com>
2026-04-27 15:19:54 +0300

    Delete studio directory

--- studio/assets/site.js
+++
@@ -1,7 +0,0 @@
-﻿document.querySelectorAll("img").forEach((img) => {
-  img.loading = "lazy";
-  img.referrerPolicy = "no-referrer";
-  img.addEventListener("error", () => {
-    img.src = "https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&w=1400&q=80";
-  }, { once: true });
-});
